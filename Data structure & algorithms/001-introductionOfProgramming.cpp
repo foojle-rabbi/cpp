bbd58a778757e12f857aab426d32acd90feadc8a
fojle rabbi <ddynamites71@gmail.com>
2023-08-19 22:30:51 +0600

    18th day of learning c++

---
+++ Data structure & algorithms/001-introductionOfProgramming.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Ajker ei prothom introduction video te temon advance kichu koy nai. ba ami jani nai keno kotha gula emn vabe botechi. ohh
+    hoo ami jani ami keno emn vabe kotha boltechi karon amar ghum paiya roiche prochur. ajke koyekdin dhoira keno jani prchur
+    ghumm pay. hoyto raite thik moto ghum hoy na. 
+
+    Accha jak ja houoar ta hoiche. ekhon mul kotha hocche ei tutorial e khali flowchart ar pseducode shomporke bolche. ja ami
+    agee thekei mutamuti jantam. flowchart ar ki program je kivabe flow krobe tar ekta chart ba chirto; ar psudocode hocche
+    emn e code ta korar aager procedure;
+
+    ar arekta jinish koilo programming language ki? programming language hoilo machine/computer er sathe kotha bolar ekta way
+    ba rasta ba language; tobe amra je code ba jei syntax e likhi computer kintu sheigula bujhe na; computer shudhu bujhe 0 
+    ar 1; ar compoiler kore ki oi amader code tare 0 and 1 e convert kore machine readable kore;
+    */
+    return 0;
+}
