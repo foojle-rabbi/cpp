f1a4429f1ee8de8f90098ff9668e8fc97699d6e2
fojle rabbi <ddynamites71@gmail.com>
2023-08-28 23:33:15 +0600

    27th day of learning c++

---
+++ Data structure & algorithms/practice things/05-binarySearch.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key){
+    int left = 0;
+    int right = n - 1;
+    while(left <= right){
+        int mid = (left + right) /2;
+        if(arr[mid] == key){
+            return mid;
+        }else if(arr[mid] > key){
+            right = mid -1;
+        }else{
+            
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+void display(int arr[], int n){
+    cout<<"Elements of array: ";
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    int arr[10] = {2,7,8,10,11,16,20,34,67,88};
+
+    //sort(arr[0], arr[10]) accha eita kaaj kore naa?
+
+    display(arr, 10);
+    cout<<binarySearch(arr, 10, 2)<<endl;
+    return 0;
+}
