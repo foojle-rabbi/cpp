029e1f2938e5d5f21dd051737a8a12d510570423
fojle rabbi <ddynamites71@gmail.com>
2023-09-06 23:58:15 +0600

    36th day of learning c++

--- Data structure & algorithms/18-Insertion Sort/01-insertionSort.cpp
+++ Data structure & algorithms/18-Insertion Sort/01-insertionSort.cpp
@@ -9,15 +9,17 @@ void display(int arr[], int n){
     cout<<endl;
 }
 
-void insertionSort(int arr[], int n){
-    for(int i = 0; i < n; i++){
-        int key = arr[i];
+void insertionSort(int *arr, int n){
+    for(int i = 1; i < n; i++){
+        int temp = arr[i];
         int j = i - 1;
-        while(j >= 0 && key < arr[j]){
-            arr[j+1] = arr[j];
-            j--; 
+
+        while(j >= 0 && temp < arr[j]){
+            arr[j + 1] = arr[j];
+            j--;
         }
-        arr[j + 1] = key;
+        arr[j + 1] = temp; //karon jodi while loop er vote gia kono index er value re shift kore taile eita abar oi temp value re 
+                            //tar main jaygay nia jaibo;
     }
 }
 
