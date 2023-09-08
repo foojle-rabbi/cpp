44f0a30978bc58f6510990c43ad71197b5dcc4d7
fojle rabbi <ddynamites71@gmail.com>
2023-09-08 23:43:42 +0600

    38th day of learning c++

--- Data structure & algorithms/10-solving leetcode/01-swapAlternative.cpp
+++ Data structure & algorithms/10-solving leetcode/01-swapAlternative.cpp
@@ -17,6 +17,12 @@ void swapAlter(int arr[], int n){
     }
 }
 
+/*
+Problem: swap alter;
+explanation: swap alter problem ta holo je tomare ekta array deoya thakbe oikahn theke tumi 2 ta 2 ta element re swap korba;
+mane tomare jodi deoya thake 1 2 5 9 tumi eitare banaiba 2 1 9 5. asha kori bujhte parcho;
+*/
+
 int main()
 {
     int arr[5] = {1, 2, 7, 8, 5};
