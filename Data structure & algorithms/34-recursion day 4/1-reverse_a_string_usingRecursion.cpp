e6b0564bae4dfc06836d1263e3298be7775f18f9
fojle rabbi <ddynamites71@gmail.com>
2023-10-26 23:54:41 +0600

    54th day of learning c++

--- Data structure & algorithms/34-recursion day 4/1-reverse_a_string_usingRecursion.cpp
+++ Data structure & algorithms/34-recursion day 4/1-reverse_a_string_usingRecursion.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
 using namespace std;
 
+bool isPalindrome(string s, int start, int end){
+    //base case
+    if(start > end){
+        return true;
+    }
+
+    if(s[start] != s[end]){
+        return false;
+    }else{
+        return isPalindrome(s, start++, end--);
+    }
+}
+
 int main()
 {
-    
+    //check if a string is palindrome
+    string s = "abcdeedcba";
+
+    cout << isPalindrome(s, 0, 10) << endl;
     return 0;
 }
