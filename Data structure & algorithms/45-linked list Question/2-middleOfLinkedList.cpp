9b74b0adf0c01b8aa6fc2a82c30d683f17cd9f20
fojle rabbi <ddynamites71@gmail.com>
2024-01-24 22:50:02 +0600

    70th day of learning c++

--- Data structure & algorithms/45-linked list Question/2-middleOfLinkedList.cpp
+++ Data structure & algorithms/45-linked list Question/2-middleOfLinkedList.cpp
@@ -1,8 +1,65 @@
 #include<iostream>
 using namespace std;
 
+class node{
+    public:
+    int data;
+    node* next;
+
+    //constructor
+    node(int val) : data(val), next(nullptr) {}
+};
+
+void insertAtLast(node* &head, int val){
+    node* newNode = new node(val);
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+
+    node* temp = head;
+
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+void display(node* head){
+    cout << "Elements of list: ";
+    while(head != nullptr){
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int getLen(node* head){
+    int count = 0;
+    while(head != NULL){
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+node* middleOfLL(node* head){
+    for(int i = 0; i < getLen(head) - 1; i++){
+        head = head->next;
+    }
+    return head;
+}
+
 int main()
 {
-    
+    node* head = NULL;
+    insertAtLast(head, 1);
+    insertAtLast(head, 2);
+    insertAtLast(head, 3);
+    insertAtLast(head, 4);
+    insertAtLast(head, 5);
+    insertAtLast(head, 6);
+    head = middleOfLL(head);
+    display(head);
     return 0;
 }
--- Data structure & algorithms/46-Linked List Question/1-Kreverse.cpp
+++ Data structure & algorithms/46-Linked List Question/1-Kreverse.cpp
@@ -1,8 +1,61 @@
 #include<iostream>
 using namespace std;
 
+class node{
+    public:
+    int val;
+    node* next;
+
+    //constructor
+    node(int v) : val(v), next(nullptr) {}
+};
+
+void display(node* head){
+    cout << "Elements of list: ";
+    while(head != nullptr){
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void insertAtLast(node* &head, int val){
+    node* newNode = new node(val);
+    if(head == nullptr){
+        head = newNode;
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+node* Kreverse(node* head, int k){
+    node* prev = NULL;
+    node* current = head;
+    node* forward = NULL;
+
+    for(int i = 0; i < k; i++){
+        forward = current->next;
+        current = prev;
+        prev = current;
+        forward = forward->next;
+    }
+    
+    node* newHead = Kreverse(forward)
+}
+
 int main()
 {
-    
+    node* head = NULL;
+    insertAtLast(head, 1);
+    insertAtLast(head, 2);
+    insertAtLast(head, 3);
+    insertAtLast(head, 4);
+    insertAtLast(head, 5);
+    display(head);
     return 0;
 }
