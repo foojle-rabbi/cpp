db28ed7cad315d0b0433a935ad6a4732c4dff5e3
fojle rabbi <ddynamites71@gmail.com>
2024-02-08 23:11:42 +0600

    82th day of learning c++

---
+++ Data structure & algorithms/58-Nth stacks in an array/1-NthStacks.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //eita ekhon dhortam e na cause eitar charar matha kichui bujhtechi na. ar ei ta jehetu hard er question tai ami apatoto chacchi hard question skip korte.
+    //ei hard question inshaa-allah pore abar solve korbo. but ekhon na. cause ekhon mathay dhukaite pari na. ar logic o bujhte pari na. 
+    return 0;
+}
