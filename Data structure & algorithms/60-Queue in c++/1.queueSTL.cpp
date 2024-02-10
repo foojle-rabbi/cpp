45a3e99a05d4efa3e177bcff992c433224dcd9c9
fojle rabbi <ddynamites71@gmail.com>
2024-02-10 23:56:28 +0600

    83th day of learning c++ | I was trying the online things yesterday

---
+++ Data structure & algorithms/60-Queue in c++/1.queueSTL.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+    queue<int> Q;
+
+    Q.push(5);
+    Q.push(12);
+    Q.push(42);
+
+    return 0;
+}
---
+++ Data structure & algorithms/60-Queue in c++/2-queueUsingArray.cpp
@@ -0,0 +1,69 @@
+
+#include<iostream>
+using namespace std;
+
+class queue{
+    private:
+    int *arr;
+    int rear;
+    int front;
+    int size;
+
+    public:
+    //constructor
+    queue(int n){
+        size = n;
+        arr = new int[size];
+        rear = -1;
+        front = -1;
+    }
+
+    void enqueue(int val){
+        //check if the array if full
+        if((rear + 1) % size == front){
+            cout << "Queue is full" << endl;
+            return;
+        }
+        if(front == -1){
+            front = 0;
+        }
+
+        rear = (rear + 1) % size;
+        arr[rear] = val;
+    }
+
+    void dequeue(void){
+        if(front == -1){
+            return;
+        }
+
+        if(front == rear){
+            front = rear = -1;
+        }
+
+        front = (front + 1) % size;
+    }
+
+    int display(void){
+        int i = front;
+        cout << "Elements of queue: ";
+        while(i != rear){
+            cout << arr[front] << " ";
+            front = (front + 1) % size;
+            i++;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    queue q(4);
+    q.enqueue(5);
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    q.display();
+    return 0;
+}
