8fef3828adcdba79ff30d8cb11972713162ae7c6
fojle rabbi <ddynamites71@gmail.com>
2024-02-16 23:19:26 +0600

    89th day of learning c++

---
+++ Data structure & algorithms/61-Queue question solve/6-interleaveTheFirstHalf_with_theSecondHalf.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+        Soppose you have give an queue: 11, 12, 13, 14, 15, 16, 17, 18
+        ekhon ami jodi first half er sathe second half interleave kori taile ans hobe: 11, 15, 12, 16, 13, 17, 14, 18;
+
+        And yes you can only use one stack only; YOu can not use extra space except a stack only;
+    
+    */
+    return 0;
+}
