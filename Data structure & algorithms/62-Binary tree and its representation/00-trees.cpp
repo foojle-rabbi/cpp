28b35dd2460399d0b18bd72370c0be3aec4aad72
fojle rabbi <ddynamites71@gmail.com>
2024-02-27 22:54:26 +0600

    93th day of learning c++

---
+++ Data structure & algorithms/62-Binary tree and its representation/00-trees.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Okay lets talk about tree data structure.
+    tree is a non-lenear data strucutre. tobe tree valo vabe janar age kichu terms shomporke valo vabe dharona thakte hobe jemon:
+        1. Node
+        2. root
+        3. children
+        4. parents
+        5. siblings
+        6. ancestor
+        7. descendent
+        8. leaf
+    Ei terms shomporek janle trees aro valo vabe bujhte para jabe.
+                            1
+                           / \
+                          2   3
+                         / \   \
+                        4   6   5
+    uporer chitro ta ekta tree ke represents kore. eikhane 1 hocche amar root node. 2 ar 3 hocche child node. ar 4, 5, 6 hocche amar leaf
+    node. ashole eigula otota hard kichu na just normal kichu jinish jeigula amader real life e amra use kori. tai eishob nia ar ekhon beshi
+    kichu lekhlam na. 
+
+    Ha tobe er aage jei tree porchilam ekhon oi tree ar ekhon kar tree same hoileo ami aage onek kichu e valo kre pori nai ba skip korchi ba
+    emon kichu topic ache jeigula ami jani e. eikhane aisha jantechi. jemon ami level order traversal shomporke jantam kintu oita kivabe implement
+    korte hoy sheta kintu ami jantam na. ar tachara shudhu ekta function (buildTree) dia kemone pura tree build korte hoy sheta o jani na
+    tai ekhon ei topic gula valo kore ayotto korte hobe. ar topics gula te valo kore time nia shikhte hobe. 
+
+    Eikhaner moddhe ekta jinish amare khub facinate korche sheta holo recursion er use kore tree build kora. Vai jotoi age recursion jani na keno
+    recursion je vai marattok ekta jinish. 
+    */
+    return 0;
+}
