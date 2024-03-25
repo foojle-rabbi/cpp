bceeda4f7a92676f481932c9ee24bfdbd1372521
fojle rabbi <ddynamites71@gmail.com>
2024-03-25 23:18:58 +0600

    109th day of learning c++

--- Data structure & algorithms/67-Burn Tree/BurnTree.cpp
+++ Data structure & algorithms/67-Burn Tree/BurnTree.cpp
@@ -1,8 +1,36 @@
 #include<iostream>
 using namespace std;
 
+class solution{
+    /*
+    solving algo: ja bujhlam solving also is pretty easy; cause there are only 3 step. yes also each step has others steps also. but primarily there are 3 steps. 
+        1. create nodeToParent mapping
+        2. find the target node
+        3. burn the tree in minimum time.
+    
+    ar ekta kotha holo gia eikhane amra level order traversal use korbo. toh eikhane 3 ta function houar kotha. but amra jokhon mapping create korbo thik tokhon e amra tagret node
+    find korbo. then amader 3rd step er kaj cholbe. 
+    */
+
+    //step 1: accha eitare pore nijer moto kore solve kori. jate matha khatate pari ar mathey o jay, ki bolo? 
+
+};
+
 int main()
 {
+    /*  
+                    1
+                   / \
+                 2     3
+                / \     \
+               4    5    6
+                   / \     \
+                  7   8     9 
+                             \
+                              10
     
+    ekhon target node jodi 8 hoy taile puro tree burn korte koto shomoy lagbe? at a time ekta node er neighbour joto ta node ache shob gula bun hobe each secend. 
+
+    */
     return 0;
 }
---
+++ Data structure & algorithms/68-Morris Traversal/flatten-a-BT-into-LL.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+                     1
+                    / \
+                   2   5
+                  / \    \          
+                 3   4     6
+                <--this to this-->
+                 1
+                  \
+                   2
+                    \ 
+                     3
+                      \ 
+                       4
+                        \ 
+                         5   
+                          \
+                           6
+
+    
+    
+    */
+    return 0;
+}
