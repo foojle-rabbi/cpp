8711eee2ac4b6339a7bc4bc84d8b2aa16ef932e0
fojle rabbi <ddynamites71@gmail.com>
2024-04-19 23:43:57 +0600

    114th day of learning c++

--- Data structure & algorithms/72-Merge two BST/mergeTwoBSt.cpp
+++ Data structure & algorithms/72-Merge two BST/mergeTwoBSt.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main()
 {
+    /*
+    Question ta motamuti interesting e. karon ei ekta question korte gia aro onek concept er valo dharona thakte hobe. tobe first ei eitar ekta brute force
+    hocche gia je 
+
+    approach #1: Accha ei approach e first 2 ta BST er inoder 2 ta vector e store kora then oi 2 ta vector ke merge kora then sorted merged vector theke BST create
+    kora. jei ta easy and simple. but at the cost of complexity O(m + n);a
+
+    ekhon amra chacchi na etto ta complexity te korar. amra chacchi jate kore constant space e amader problem ta solve hoy jay. toh shei tao kora jabe but koyekta
+    concept clear thaka joruri. jemon: bst to linked list. merge two linked list. linked list to bst.
+
+    ashole hisabe ami pura problem solve korar way e bole diyecchi. 
+
+    approach #2: approach number 2 ta kichu lengthy but optimal. toh eikhaner moddhe 3 ta step ache ei step gula follow korlei prblem will be solved. theese three
+    steps are: 
+        1. convert both bst in to a sorted doulbly linked list
+        2. merge these two sorted doubly linked list.
+        3. Convert the merged list into a BST. 
+
     
+    */
 
     
     return 0;
