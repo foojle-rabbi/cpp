134c18bdae00123ebfa85e21e252d427c4ce1b1f
fojle rabbi <ddynamites71@gmail.com>
2024-05-17 23:55:08 +0600

    128th day of learning c++

--- Data structure & algorithms/85-Introduction of Graph/adjacency-of-list.cpp
+++ Data structure & algorithms/85-Introduction of Graph/adjacency-of-list.cpp
@@ -2,8 +2,38 @@
 #include<vector>
 using namespace std;
 
+void addEdge(vector<int> adj[], int u, int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+
+    cout << "Add edge function called\n";
+}
+
+void printGraph(vector<int> adj[], int v){
+    cout << "Adjacency of list: ";
+    for(int i = 0; i < v; i++){
+        cout << i << "--> ";
+        for(auto j : adj[i]){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << "print function called";
+
+}
+
 int main()
-{
-    vector<vector<int> adj;
+{   
+    cout << "main function called\n";
+    int v = 4;
+    vector<vector<int> > adj;
+
+    // addEdge(adj, 1, 2);
+    // addEdge(adj, 1, 3);
+    // addEdge(adj, 2, 3);
+    // addEdge(adj, 3, 4);
+    cout << "Came to 35 number line\n";
+
+    printGraph(adj, v);
     return 0;
 }
--- Data structure & algorithms/85-Introduction of Graph/adjacent-of-matrix.cpp
+++ Data structure & algorithms/85-Introduction of Graph/adjacent-of-matrix.cpp
@@ -1,19 +1,48 @@
 #include<iostream>
 using namespace std;
 
+int adj[10][10]; //globally initialize korar motive hocche jate array er shob gula value amar 0 dia initialize hoy. 
 class grpah{
-    private: 
-    int adj[10][10];
-
     public:
+    int nodes, edges;
     void takeInput(){
-        int nodes, edges;
-        cout 
+        cout << "Enter the number of nodes and edges: ";
+        cin >> nodes >> edges;
+
+        int a, b;
+        for(int i = 0; i < nodes; i++){
+            cin >> a >> b;
+            adj[a][b] = 1;
+            adj[b][a] = 1;
+        }
+    }
+
+    void print(void){
+        for(int i = 0; i <= nodes; i++){
+            for(int j = 0; j <= nodes; j++){
+                cout << adj[i][j] << "\t";
+            }
+            cout << endl;
+        }
     }
 };
 
+/*
+Graph data structure shomporke toh janlam e. ekhon kotha hocche gia ei graph ke toh 2 way te represent kora jay. one is
+adjacent of list ar arekta hocche adjacent of matrix. ekhon eikhane ami adjacent of matrix er represntaion ta dekhailam. but
+in realiy matrix use kora hoy na. karon 2d array nile amader onek space hudai waste hoy. she jonno amder adjacent of list tarei
+beshi preffer kora hoy. onek jaygay toh matrix re dekhai e na. 
+
+
+kintu tarpor o ami jokhon c korchilam tokhon eita shikhechilam. aar shejonno e sheitar moto kore eikhane implemnt korlam. 
+*/
+
 int main()
 {
-    
+    grpah g;
+    g.takeInput();
+    g.print();
     return 0;
 }
+
+//output string: 4 4 1 2 1 3 2 3 3 4 
