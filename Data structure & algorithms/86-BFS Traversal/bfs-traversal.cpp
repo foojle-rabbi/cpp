2bad42ac15aac27d17b8dc479c6687c75916fb4c
fojle rabbi <ddynamites71@gmail.com>
2024-07-02 23:23:21 +0600

    133th day of learning c++

---
+++ Data structure & algorithms/86-BFS Traversal/bfs-traversal.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<queue>
+using namespace std;
+
+class grpah{
+    public:
+    unordered_map<int, list<int> > adj;
+    int vertices, num1, num2;
+    unordered_map<int, bool> visited;
+
+    void takeInput(void){
+        cout << "Enter the number of vertices: ";
+        cin >> vertices;
+        //first e tomar graph er moddhe koto ta vertice ache tar number ta nia neo. jate kore ekta loop use kore shob gula 
+        //vetice input akare noeya jay. 
+
+        cout << "Enter the input string: ";
+        while (vertices--)
+        {
+            //jei jei node er moddhe connection ache shei 2 ta number input hisabe neo. then list er moddhe push back koro. 
+            cin >> num1 >> num2;
+            adj[num1].push_back(num2);
+            adj[num2].push_back(num1); // jehetu kono direction nai tai 2 tar moddhei push back kora lagbe. 
+        }
+    }
+
+    void display(void){
+        cout << "Adjacent list: " << endl;
+
+        for(auto i : adj){
+            cout << i.first << " --> ";
+            for(auto j : i.second){
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    /*
+    Vai bfs traversal o emon ahamori kothin kichu na. eita tor onekta tree er level order traversal er motoi. orthat first er
+    je kono ekta source nibi. then oi source tare ekta queue te push korbo. then eitare por korar por print korbi. ar dekhbi
+    eitar sathe connected ache. jeigula conncted ache oigulare push korbi queue. but eikhane ekta jinish jate mathay thake je
+    amra each time shudhu ekta node re ekbar e print korbi. tai shei track rakhar jonno dorkar ekta visited array, vector, orthoba
+    ekta map. bach eituku e. 
+    */
+
+    void bfs(int source){
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+
+        cout << "Output of bfs traversal: ";
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+
+            cout << front << " ";
+
+            for(auto i : adj[front]){
+                if(visited[i] != true){
+                    q.push(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    grpah g;
+    g.takeInput();
+    g.display();
+    g.bfs(0);
+    return 0;
+}
---
+++ Data structure & algorithms/87-DFS Traversal/dfs.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Dfs mane Deapth first search. orthat aage tmi deapth e jao then traverse koro. ar deapth first seach e amora mainly 
+    recursive function er use korbo. ashole ahamori temon kothin kono tai nai. just khali ektu monojog dia porlei easy lage
+    but jodi monojog na thake tailei main problem. 
+
+    
+    */
+    return 0;
+}
