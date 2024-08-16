3eb761c7420cf7e64857e066223a541e8d971f93
fojle rabbi <ddynamites71@gmail.com>
2024-08-16 23:37:43 +0600

    148th day of learning c++

---
+++ Data structure & algorithms/95-Dijkstra's Algorithms/dijkstras.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    Shono vaiye ei algorithm tao etto kothin kichu na. oije coder army er lecturer koilo na, je eitao emon ahamori hard 
+    kono problem na. amra just amader common sense use korbo to solve this problem. asholei tai amra amder common sense use
+    korlei ei problem ta solve korte parbo.
+
+    tobe ekta notun jinish ki janlam janosh fojle, je alogrithm manei kintu problem solve korar steps. toh amra jodi kono
+    problem re aage divide kore felte pari je kivabe solve kore ba koto ta step follow kore hobe taholei amader problem gula
+    khub easily solve hoy + mone rakhao easy hoiya jay.
+
+    -----
+
+    toh ekhon ashi ei dijkstra algorithm ta ashole ki? eitao ashole emon hard kichu na. problem ta hocche tomare ekta undirectred
+    grpah deoya thakbe. ha hoite pare directed o. sathe ekta source node deoya thakbe tomake oi source node theke onnanno node
+    er shortest path er vector/array retruen korte hobe. kenono ekta source node theke others node e jeete kintu onekgula e 
+    path thake toh oikhan theke tomake shobchey minimum distance ta ber kore diba. jar name e hocche dijkstra's algorithms.
+
+    dekho eita solve korar ekta way just love babbar mia dekhaiche. ar coder army mia eita solve korar 2 ta method dekhaiche. 
+    ar eikhan theke arekta realization hocche gia je coder army onek shundor kore poray + theory + ekkebare basic basic jinish
+    gulao poray. 
+
+    orthat kono ekta jinish keno use kormu, keno use korbo na. ba kokhon konta use korte hobe sheita. 
+
+    toh prothome jei approach ta dekhaiche oita'r moddhe time complexity onek beshi. onek beshi bolte v^2. jeitar moddhe just
+    2 ta data structure ekta use kora hoiche. first distance and second is explored/visited.
+
+    ----------------- solving approch ----------------------
+    1. first jeehtu amader shob gula node re niyai kaj korte hobe she jonno ekta loop cholbe v times.
+    2. then 2 ta vector initialize korba. ekta explored arekta distance
+    3. then first e source node er distance = 0 rakhba.
+    4. then loop er vitore arekta loop chalaiba eita dekhar jonno jonno je distance vector er moddhe shob cheye minimum 
+        value koto. then oi value mane node ta niba.
+    5. oi node er joto gula neighbour age shegular sathe current node er edges er value + korba.
+    6. jodi + korar por ager value theke choto hoy tahole update korba. ar na hobe ignore. 
+
+    ajke raat onek hoche + laptop er display te mone arekta line uthbe jani na. but problem dekha diteche. uthle toh ar kichu
+    korar nai. joto din chole arki. jai ghumai ga. gia. 
+
+
+    */
+    return 0;
+}
