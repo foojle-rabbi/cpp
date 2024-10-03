ab571199b97260e027b87b7bdd45c71b45024cff
fojle-rabbi <ddynamites71@gmail.com>
2024-10-03 01:09:58 -0600

    After setting up a new win version

--- Data structure & algorithms/95-Dijkstra's Algorithms/dijkstrasAlgo.cpp
+++ Data structure & algorithms/95-Dijkstra's Algorithms/dijkstrasAlgo.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /*
 kichu kotha: 
 vaba jay ami jast 18 july programming korchi. orthat ei 15 diner moto ami laptop er samne boshi e nai. ar na kichu shikhte parchi
-er ageo kintu onek bar ami porashuna bad diya faizlai korchi ar shomoy waste korchi. Ei year tate ami time khub beshi e waste 
+er ageo kintu onek bar ami porashuna bad diya faizlami korchi ar shomoy waste korchi. Ei year tate ami time khub beshi e waste 
 korchi re vai khub beshi e waste korchi. ar tar upor jei koyekdin e porchi sheigula o mono jog sohokare porte pari nai. 
 
 jak allah amare beshi beshi kore porashona korar toufik dan koruk, shokole boli amin, amin, amin.
