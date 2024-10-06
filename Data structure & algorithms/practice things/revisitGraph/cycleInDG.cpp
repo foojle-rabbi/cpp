31a5a7b50edd5835d455437e3300be3a3f29b968
fojle-rabbi <ddynamites71@gmail.com>
2024-10-06 10:28:16 -0600

    revisited cycle detection for more clarity

---
+++ Data structure & algorithms/practice things/revisitGraph/cycleInDG.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+/*
+কালকে সম্ভবত আমরা আনডাইরেক্টেড গ্রাফের মধ্যে কিভাবে সাইকেল ডিটেক্ট করতে হয় ঐটা রিভিশন দিয়েছিলাম। আজকে আমরা কিভাবে ডাইরেক্টেড
+গ্রাফ এর মধ্যে সাইকেল ডিটেক্ট করতে হয় সেটা আবার দেখব। 
+
+আচ্ছা কোড করার আগে আমি আমার পারস্পেকটিভ টা সেয়ার করি। আমার যে এই এলগরিদম টা মনে নাই বিষয়টা এমন ও না। আমার প্রায় পুরোটা লজিক 
+ই মনে ছিলো, এখন দেখার পরে আরো ইজি হইয়া গেলো। 
+
+accha english e lekhi. main kotha hocche directed graph er jonno amra eikhane dfs algorithm use korbo. bfs dia o kora jay. 
+but easy hocche dfs dia. jehetu eita ekta recursive function. toh logic ta hocche amar ekta extra mapping dorkar jeitar nam
+hobe dfs visited. jodi visited hoy + dfs visited hoy. means 2 tai true hoy tahole cycle present. ar jodi na hoy tahole cycle 
+nai. tobe call theke ber hoyar shomoy jeno dfsvisited re false kore deoya hoy. 
+*/
+
+int main()
+{
+    /*
+    Jak eikhane ar notun kore code ture anlam na. but eikhaner main jei logic shei ta hocche gia. jodi 
+    amar graph e cycle thake tahole shei node gula dfs visited e thakbe ar nodi cycle na thake tahole amar oi
+    node gula dfsVisited thakbe na. ar dfs visited er kaj hocche current call stack tare dhoira rakha. orthat
+    amar joto gula nodes ek stahe connected ache shei shob gulare dfs visited e rakha. 
+    */
+    return 0;
+}
