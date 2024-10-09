36a77db499bb9be18d95b3780fd7ec5159b1cab7
fojle-rabbi <ddynamites71@gmail.com>
2024-10-09 11:59:36 -0600

    revisiting the shortest paths

---
+++ Data structure & algorithms/practice things/revisitGraph/shortestPathInUdg.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
+
+/*
+Accha ei question ta onek age ekbar age korchilam. ar ajke aisha abar revision dite ashci. ar ei bar graph er jei question 
+gula ache eigula ami oto ta valo vabe bujhtam na. bujhi o nai. er age quetion solve kore submit o korchi and accept 
+o hoiche. kintu kono dry run kori nai. jar karone ekhon aisha vuila jaitechi. 
+
+doro ei je question ta eita kintu kora hoiche. kintu jei logic shei ta vuila gechilam. kintu ekhon dekhar por aisha abar 
+mone hoiche. toh eitar logic ta hocche ekta extra vector rakhte hobe for tracking parents. then oikhan theke kivabe kore
+oita purapuri mone nai 
+
+she jonno e dry run korte hobe. jeta insha-allah kalke korbo. ajke amma aschilo tai ektu kotha hoichio. ar shorirer 
+condition o temon valo na. 
+*/
